92a60e86157608490462748e3a8c6e4b19a6feb0
ricardovaldesv <ricardovaldes@icloud.com>
2023-12-20 23:29:49 +0000

    task 0 v11

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,62 +1,30 @@
 #!/usr/bin/python3
-"""
-given employee ID, returns information about his/her todo list progress.
-"""
+"""Gather data about completed tasks for specific user"""
+
 import requests
 import sys
 
-
-def display_employee():
-    """
-    given employee ID, returns information about his/her todo list progress.
-    """
-    employee_id = sys.argv[1]
-    todos_url = 'https://jsonplaceholder.typicode.com/todos'
-    users_url = 'https://jsonplaceholder.typicode.com/users'
-
-    todos = requests.get(todos_url)
-    users = requests.get(users_url)
-
-    if todos.status_code == 200 and users.status_code == 200:
-        todos_data = todos.json()
-        users_data = users.json()
-
-        # Create a dictionary for mapping user IDs to names
-        user_names = {user['id']: user['name'] for user in users_data}
-
-        completed_tasks = []
-
-        # Get the completed tasks for the specific user
-        for task in todos_data:
-            user_id = task['userId']
-            completed = task['completed']
-            title = task['title']
-
-            if user_id == int(employee_id) and completed:
-                completed_tasks.append(title)
-
-        # Print the employee's completed tasks
-        if int(employee_id) in user_names:
-            EMPLOYEE_NAME = user_names[int(employee_id)]
-            NUMBER_OF_DONE_TASKS = len(completed_tasks)
-            TOTAL_NUMBER_OF_TASKS = len([
-                                         task for task in todos_data if task
-                                         ['userId'] == int(employee_id)
-                                         ])
-
-            print(f"Employee {EMPLOYEE_NAME} is done with tasks"
-                  f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
-
-            # Print completed tasks in a list
-            for TASK_TITLE in completed_tasks:
-                print(f"\t {TASK_TITLE}")
-        else:
-            print("No employee was found with that ID.")
-
-    else:
-        print("The information could not be obtained from the server.")
-
-
 if __name__ == "__main__":
 
-    display_employee()
+    if sys.argv[1].isdigit():
+
+        user_id = sys.argv[1]
+        employee = requests.get('https://jsonplaceholder.typicode.com/users',
+                                params={'id':  user_id})
+        tasks = requests.get('https://jsonplaceholder.typicode.com/todos',
+                             params={'userId':  user_id})
+
+        user_name = employee.json()[0].get('name')
+        total_tasks = len(tasks.json())
+        done_tasks = 0
+
+        # count done tasks
+        for task in tasks.json():
+            if task.get('completed'):
+                done_tasks += 1
+
+        print("Employee {} is done with tasks({}/{}):\
+    ".format(user_name, done_tasks, total_tasks))
+        for task in tasks.json():
+            if task.get('completed'):
+                print("\t {}".format(task.get('title')))
