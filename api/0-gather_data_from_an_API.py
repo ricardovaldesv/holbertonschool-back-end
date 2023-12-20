5da8ba618bec2d7bd3807068c22a14c617aefbf0
ricardovaldesv <ricardovaldes@icloud.com>
2023-12-20 23:35:17 +0000

    task 0 v13

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,51 +1,62 @@
 #!/usr/bin/python3
 """
-Python script that, using this REST API, for a given employee ID,
-returns information about his/her todo list progress.
+given employee ID, returns information about his/her todo list progress.
 """
 import requests
-from sys import argv
+import sys
 
 
-def display_info():
-    '''
-    This is the function to serch for a user info and tasks.
-    '''
+def display_employee():
+    """
+    given employee ID, returns information about his/her todo list progress.
+    """
+    employee_id = sys.argv[1]
+    todos_url = 'https://jsonplaceholder.typicode.com/todos'
+    users_url = 'https://jsonplaceholder.typicode.com/users'
 
-    employee_id = str(argv[1])
+    todos = requests.get(todos_url)
+    users = requests.get(users_url)
 
-    users_url = "https://jsonplaceholder.typicode.com/users/" + employee_id
+    if todos.status_code == 200 and users.status_code == 200:
+        todos_data = todos.json()
+        users_data = users.json()
 
-    users_response = requests.get(users_url)
+        # Create a dictionary for mapping user IDs to names
+        user_names = {user['id']: user['name'] for user in users_data}
 
-    if users_response.status_code == 200:
-        users_data = users_response.json()
-        user_name = users_data.get("name")  # Name of the user
+        completed_tasks = []
 
-    todos_url = "https://jsonplaceholder.typicode.com/todos/"
+        # Get the completed tasks for the specific user
+        for task in todos_data:
+            user_id = task['userId']
+            completed = task['completed']
+            title = task['title']
 
-    todos_response = requests.get(todos_url)
+            if user_id == int(employee_id) and completed:
+                completed_tasks.append(title)
 
-    if todos_response.status_code == 200:
-        todos_data = todos_response.json()
-        list_complete_task = []
-        num_of_task = 0
-        num_of_complete_task = 0
-        for dict in todos_data:
-            if dict.get("userId") == int(employee_id)\
-               and dict.get("completed"):
-                list_complete_task.append(dict.get("title"))
-                num_of_complete_task += 1
-            if dict.get("userId") == int(employee_id):
-                num_of_task += 1
-    # print(f"Employee {user_name} is done
-    # with tasks({num_of_complete_task}/{num_of_task}):")
-    all_text = f"Employee {user_name} is done with tasks"
-    print(f"{all_text}({num_of_complete_task}/{num_of_task}):")
-    for task in list_complete_task:
-        print(f"\t {task}")
+        # Print the employee's completed tasks
+        if int(employee_id) in user_names:
+            EMPLOYEE_NAME = user_names[int(employee_id)]
+            NUMBER_OF_DONE_TASKS = len(completed_tasks)
+            TOTAL_NUMBER_OF_TASKS = len([
+                                         task for task in todos_data if task
+                                         ['userId'] == int(employee_id)
+                                         ])
+
+            print(f"Employee {EMPLOYEE_NAME} is done with tasks"
+                  f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+
+            # Print completed tasks in a list
+            for TASK_TITLE in completed_tasks:
+                print(f"\t {TASK_TITLE}")
+        else:
+            print("No employee was found with that ID.")
+
+    else:
+        print("The information could not be obtained from the server.")
 
 
 if __name__ == "__main__":
 
-    display_info()
+    display_employee()
