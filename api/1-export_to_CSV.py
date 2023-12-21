c560f7e6ea852928c77e7215f278d8342b0cbdb5
ricardovaldesv <ricardovaldes@icloud.com>
2023-12-21 00:30:17 +0000

    task 1 V1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,18 +1,9 @@
-#!/usr/bin/python3
-"""
-Given employee ID, exports information about
-his/her todo list progress to a CSV file.
-"""
 import csv
 import requests
 import sys
 
 
 def export_to_csv(employee_id):
-    """
-    Given employee ID, exports information about his/her
-    todo list progress to a CSV file.
-    """
     todos = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
     users_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
 
@@ -23,21 +14,31 @@ def export_to_csv(employee_id):
         todos_data = todos.json()
         user_data = user.json()
 
-        user_name = user_data['username']
+        if isinstance(user_data, list) and len(user_data) > 0:
+            user_name = user_data[0]['username']
+        elif isinstance(user_data, dict):
+            user_name = user_data.get('username', '')
+        else:
+            user_name = ''
 
         filename = f"{employee_id}.csv"
 
         with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
             csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-            csv_writer.writerow(['UserID', 'username', 'Completed', 'Title'])
+
+            rows = [
+                ['UserID', 'username', 'Completed', 'Title']
+            ]
 
             for task in todos_data:
-                csv_writer.writerow([
+                rows.append([
                     str(task['userId']),
                     user_name,
                     str(task['completed']),
                     task['title']
                 ])
+
+            csv_writer.writerows(rows[1:])
     else:
         print("Failed to fetch data from the server.")
 
